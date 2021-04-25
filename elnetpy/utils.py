a12411f2fe7b3ee4b70cfb983bfe4d96c5e8c510
Pablo Rodriguez Mira <Rodriguez.Mira.Pablo@gmail.com>
2021-04-25 23:06:03 +0200

    refactor standardize_inputs and add functions to destandardize coefficients and get lambda path + pytests

--- elnetpy/utils.py
+++ elnetpy/utils.py
@@ -4,9 +4,44 @@ import numpy as np
 from scipy.stats import zscore
 
 
-def standardize_inputs(X, y, lambdas):
+def standardize_inputs(X, y, return_means_stds=False):
+    if return_means_stds:
+        X_means = X.mean(axis=0)
+        X_stds = X.std(axis=0)
+        y_mean = np.mean(y)
+        y_std = np.std(y)
     X = zscore(X)
-    y_std = np.std(y)
     y = zscore(y)
-    lambdas = lambdas / y_std
-    return X, y, lambdas
+    return (X, y, X_means, X_stds, y_mean, y_std) if return_means_stds else (X, y)
+
+
+def destandardize_coefs(coefs_mat, X_means, X_stds, y_mean, y_std):
+    """Destandardize elastic net (standardized) coefficients
+
+    :param coefs_mat: Matrix of coefficients of shape (n_features, n_lambdas)
+    :type coefs_mat: numpy.array
+    :param X_stds: Means of the features of shape (n_features,)
+    :type X_std: numpy.array
+    :param X_stds: Standard deviations of the features of shape (n_features,)
+    :type X_std: numpy.array
+    :param y_mean: Mean of the response
+    :type y_mean: float
+    :param y_std: Standard deviation of the response
+    :type y_std: float
+
+    :return: Coefficient matrix of shape (n_features, n_lambdas) and intercepts vector
+    of shape (n_lambdas,)
+    :rtype: tuple
+    """
+    coefs_mat_destd = (coefs_mat * y_std) / X_stds[:, None]
+    intercepts = y_mean - coefs_mat_destd.T.dot(X_means)
+    return coefs_mat_destd, intercepts
+
+
+def get_lambda_path(X, y, alpha, min_lambda_ratio, n_lambda):
+    n_obs = X.shape[0]
+    lambda_max = np.max(np.abs(X.T.dot(y))) / n_obs
+    lambda_min = lambda_max * min_lambda_ratio
+    # sequence on the log scale
+    lambda_path = np.exp(np.linspace(np.log(lambda_min), np.log(lambda_max), n_lambda))
+    return lambda_path
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-from elnetpy.utils import standardize_inputs
+from elnetpy.utils import standardize_inputs, destandardize_coefs
 
 
 def test_standardize_inputs():
@@ -14,7 +14,6 @@ def test_standardize_inputs():
         ]
     )
     y = np.array([10, 32, 12])
-    lambdas = np.array([0, 0.5, 1])
     expected_X = np.zeros([3, 3])
     for j in range(X.shape[1]):
         col = X[:, j]
@@ -24,8 +23,63 @@ def test_standardize_inputs():
     expected_y = y - np.mean(y)
     y_std = np.sqrt(np.mean(expected_y ** 2))
     expected_y = expected_y / y_std
-    expected_lambdas = lambdas / y_std
-    Xt, yt, lambdast = standardize_inputs(X, y, lambdas)
+    Xt, yt = standardize_inputs(X, y)
     np.testing.assert_equal(Xt, expected_X)
     np.testing.assert_equal(yt, expected_y)
-    np.testing.assert_equal(lambdast, expected_lambdas)
+
+
+def test_standardize_inputs_stds():
+    X = np.array(
+        [
+            [1, 2, 3],
+            [-1, 5, 10],
+            [-10, 2, 8],
+        ]
+    )
+    y = np.array([10, 32, 12])
+    expected_X = np.zeros([3, 3])
+    expected_X_stds = np.zeros(3)
+    expected_X_means = np.zeros(3)
+    for j in range(X.shape[1]):
+        col = X[:, j]
+        expected_X_means[j] = np.mean(col)
+        col = col - expected_X_means[j]
+        expected_X_stds[j] = np.sqrt(np.mean(col ** 2))
+        col = col / expected_X_stds[j]
+        expected_X[:, j] = col
+    expected_y_mean = np.mean(y)
+    expected_y = y - expected_y_mean
+    expected_y_std = np.sqrt(np.mean(expected_y ** 2))
+    expected_y = expected_y / expected_y_std
+    Xt, yt, X_means, X_stds, y_mean, y_std = standardize_inputs(
+        X, y, return_means_stds=True
+    )
+    np.testing.assert_equal(Xt, expected_X)
+    np.testing.assert_equal(yt, expected_y)
+    np.testing.assert_equal(X_means, expected_X_means)
+    np.testing.assert_equal(X_stds, expected_X_stds)
+    assert y_mean == expected_y_mean
+    assert y_std == expected_y_std
+
+
+def test_destandardize_coefs():
+    # simulated standardized coefficients for 5 features and 100 lambdas
+    coefs_mat = np.random.normal(size=(5, 100))
+    X_means = np.array([3, 2, 5, 1, 0])
+    X_stds = np.array([2, 0.5, 1, 5, 0.2])
+    y_mean = 12
+    y_std = 0.4
+    expected_coefs_mat_destd = coefs_mat.copy()
+    expected_intercepts = np.zeros(100)
+    for k in range(coefs_mat.shape[1]):
+        expected_intercepts[k] = y_mean
+        for j in range(coefs_mat.shape[0]):
+            expected_coefs_mat_destd[j, k] = (
+                expected_coefs_mat_destd[j, k] * y_std
+            ) / X_stds[j]
+            expected_intercepts[k] -= expected_coefs_mat_destd[j, k] * X_means[j]
+    coefs_mat_destd, intercepts = destandardize_coefs(
+        coefs_mat, X_means, X_stds, y_mean, y_std
+    )
+    np.testing.assert_equal(coefs_mat_destd, expected_coefs_mat_destd)
+    np.testing.assert_almost_equal(intercepts, expected_intercepts)
