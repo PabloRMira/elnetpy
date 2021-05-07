defb373448a424a8c3abd7cce23df83ee66d1c61
Pablo Rodriguez Mira <Rodriguez.Mira.Pablo@gmail.com>
2021-05-07 23:01:39 +0200

    add cross validation functionality and predict function

--- elnetpy/linear.py
+++ elnetpy/linear.py
@@ -2,9 +2,19 @@
 
 import numpy as np
 from sklearn.base import BaseEstimator
+from sklearn.model_selection import KFold
+from joblib import Parallel, delayed
 
 from elnetpy import linear_elnet
-from elnetpy.utils import standardize_inputs, destandardize_coefs, get_lambda_path
+from elnetpy.utils import (
+    standardize_inputs,
+    destandardize_coefs,
+    get_lambda_path,
+    get_linear_scorer,
+    score_multiple,
+    check_fitted_model,
+    interpolate_model,
+)
 
 
 class Elnet(BaseEstimator):
@@ -16,41 +26,62 @@ class Elnet(BaseEstimator):
         lambdas=None,
         n_lambda=100,
         min_lambda_ratio=1e-4,
+        n_splits=1,
+        scoring="r2",
         n_jobs=1,
         tol=1e-7,
         max_iter=1e5,
+        random_state=182,
     ):
         self._validate_input(
-            alpha, lambdas, n_lambda, min_lambda_ratio, n_jobs, tol, max_iter
+            alpha,
+            lambdas,
+            n_lambda,
+            min_lambda_ratio,
+            n_jobs,
+            tol,
+            max_iter,
+            n_splits,
+            scoring,
+            random_state,
         )
         self.alpha = alpha
         self.lambdas = lambdas
         self.n_lambda = n_lambda
         self.min_lambda_ratio = min_lambda_ratio
+        self.n_splits = n_splits
+        self.scoring = scoring
         self.n_jobs = n_jobs
         self.tol = tol
         self.max_iter = max_iter
+        self.random_state = random_state
 
     def fit(self, X, y):
-
-        self._fit(X, y)
-
-        return self
-
-    def _fit(self, X, y):
 
         # for matrices to be passed by reference with pybind11
         # we need Fortran column major storage order
         _X = X.astype(dtype="float64", order="F", copy=True)
         _y = y.astype(dtype="float64", copy=True)
 
-        _X, _y, X_means, X_stds, y_mean, y_std = standardize_inputs(
-            _X, _y, return_means_stds=True
+        # fit on the whole dataset
+        self._fit(_X, _y)
+
+        # cross validation
+        if self.n_splits > 1:
+
+            self._fit_cv(_X, _y)
+
+        return self
+
+    def _fit(self, X, y):
+
+        X, y, X_means, X_stds, y_mean, y_std = standardize_inputs(
+            X, y, return_means_stds=True
         )
 
         if self.lambdas is None:
             lambda_path_std = get_lambda_path(
-                _X, _y, self.alpha, self.min_lambda_ratio, self.n_lambda
+                X, y, self.alpha, self.min_lambda_ratio, self.n_lambda
             )
             self.lambda_path_ = lambda_path_std * y_std
         else:
@@ -65,7 +96,7 @@ class Elnet(BaseEstimator):
         # get standardized coefficients
         early_stopping = True
         coefs_mat = linear_elnet(
-            _X, _y, lambda_path_std, self.alpha, early_stopping, self.tol, self.max_iter
+            X, y, lambda_path_std, self.alpha, early_stopping, self.tol, self.max_iter
         )
 
         # destandardize coefficients and get intercepts
@@ -78,9 +109,99 @@ class Elnet(BaseEstimator):
 
         return self
 
+    def _score_one(
+        self, X, y, lambda_path, alpha, train_idx, test_idx, scorer, tol, max_iter
+    ):
+
+        X, y, X_means, X_stds, y_mean, y_std = standardize_inputs(
+            X, y, return_means_stds=True
+        )
+
+        # standardize lambdas
+        lambda_path_std = lambda_path / y_std
+
+        # get standardized coefficients
+        coefs_mat = linear_elnet(X, y, lambda_path_std, alpha, False, tol, max_iter)
+        y_preds_mat = X.dot(coefs_mat)
+        scores = score_multiple(y, y_preds_mat, scorer)
+        return scores
+
+    def _fit_cv(self, X, y):
+
+        splits = KFold(n_splits=self.n_splits, random_state=self.random_state).split(X)
+
+        scorer_dict = get_linear_scorer[self.scoring]
+        scorer = scorer_dict["scorer"]
+        greater_better = scorer_dict["greater_better"]
+
+        scores = Parallel(n_jobs=self.n_splits)(
+            delayed(self._score_one)(
+                X,
+                y,
+                self.lambda_path_,
+                self.alpha,
+                train_idx,
+                test_idx,
+                scorer,
+                self.tol,
+                self.max_iter,
+            )
+            for (train_idx, test_idx) in splits
+        )
+        scores = np.array(scores) if greater_better else -np.array(scores)
+        self.cv_mean = scores.mean(axis=0)
+        self.cv_std = scores.std(axis=0)
+        self.cv_up = self.cv_mean + self.cv_std
+        self.cv_low = self.cv_mean - self.cv_std
+
+        lambda_best_idx = np.argmax(self.cv_mean)
+        self.lambda_best_ = self.lambda_path_[lambda_best_idx]
+
+        lambda_1std_idx = int(np.argwhere(self.cv_up >= self.cv_mean))
+        self.lambda_1std_ = self.lambda_path_[lambda_1std_idx]
+
+        self.coef_ = self.coef_path_[:, self.lambda_1std_]
+        self.intercept_ = self.intercept_path_[self.lambda_1std_]
+
+        return self
+
+    def predict(self, X, lamb=None):
+
+        check_fitted_model(self)
+
+        if lamb is None and not hasattr(self, "lambda_1std_"):
+            raise Exception(
+                """Please provide a lamb value for predict
+                   or fit the estimator via cross validation
+                   to use lambda_1std as default"""
+            )
+        elif lamb is None and hasattr(self, "lambda_1std_"):
+            coef = self.coef_
+            intercept = self.intercept_
+        else:
+            coef, intercept = interpolate_model(
+                lamb, self.lambda_path, self.coef_path_, self.intercept_path_
+            )
+
+        y_pred = X.dot(coef) + intercept
+
+        if y_pred.shape[1] == 1:
+            y_pred = y_pred.squeeze()
+
+        return y_pred
+
     @staticmethod
     def _validate_input(
-        alpha, lambdas, n_lambda, min_lambda_ratio, n_jobs, tol, max_iter
+        alpha,
+        lambdas,
+        n_lambda,
+        min_lambda_ratio,
+        n_jobs,
+        tol,
+        max_iter,
+        n_splits,
+        scoring,
+        random_state,
     ):
         if alpha < 0 or alpha > 1:
             raise ValueError(
@@ -116,3 +237,15 @@ class Elnet(BaseEstimator):
                 f"""max_iter should be greater than or equal 100
                 Your input max_iter is {max_iter}"""
             )
+        if n_splits <= 0 or n_splits >= 100:
+            raise ValueError(
+                f"""n_splits should be between 1 and 100
+                Your input n_splits is {n_splits}"""
+            )
+        if not isinstance(scoring, str) or scoring not in get_linear_scorer.keys():
+            raise ValueError(
+                f"""Only {", ".join(get_linear_scorer.keys())} are implemented
+                Your input scoring is {scoring}"""
+            )
+        if not isinstance(random_state, int):
+            raise ValueError("random_state should be an integer")
