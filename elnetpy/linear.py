fix lambda 1se calculation

elnetpy/linear.py
@@ -135,18 +135,17 @@ class Elnet(BaseEstimator):
         )
         scores = np.array(scores) if greater_better else -np.array(scores)
         self.cv_mean = scores.mean(axis=0)
-        self.cv_std = scores.std(axis=0)
-        self.cv_up = self.cv_mean + self.cv_std
-        self.cv_low = self.cv_mean - self.cv_std
+        self.cv_se = scores.std(axis=0, ddof=1) / np.sqrt(self.n_splits)
 
-        lambda_best_idx = np.argmax(self.cv_mean)
-        self.lambda_best_ = self.lambda_path_[lambda_best_idx]
+        lambda_max_idx = np.argmax(self.cv_mean)
+        cv_max_low = (self.cv_mean - self.cv_se)[lambda_max_idx]
+        self.lambda_max_ = self.lambda_path_[lambda_max_idx]
 
-        lambda_1std_idx = int(np.argwhere(self.cv_up >= self.cv_mean)[0])
-        self.lambda_1std_ = self.lambda_path_[lambda_1std_idx]
+        lambda_1se_idx = int(np.argwhere(self.cv_mean >= cv_max_low)[0])
+        self.lambda_1se_ = self.lambda_path_[lambda_1se_idx]
 
-        self.coef_ = self.coef_path_[:, lambda_1std_idx]
-        self.intercept_ = self.intercept_path_[lambda_1std_idx]
+        self.coef_ = self.coef_path_[:, lambda_1se_idx]
+        self.intercept_ = self.intercept_path_[lambda_1se_idx]
 
         return self
 
@@ -154,13 +153,13 @@ class Elnet(BaseEstimator):
 
         check_fitted_model(self)
 
-        if lamb is None and not hasattr(self, "lambda_1std_"):
+        if lamb is None and not hasattr(self, "lambda_1se_"):
             raise Exception(
-                """Please provide a lamb value for predict
-                   or fit the estimator via cross validation
-                   to use lambda_1std as default"""
+                "Please provide a lamb value for predict "
+                "or fit the estimator via cross validation "
+                "to use lambda_1se as default"
             )
-        elif lamb is None and hasattr(self, "lambda_1std_"):
+        elif lamb is None and hasattr(self, "lambda_1se_"):
             coef = self.coef_
             intercept = self.intercept_
         else:

tests/unit_tests/test_linear.py
@@ -6,7 +6,7 @@ from glmnet import ElasticNet
 
 from elnetpy.linear import Elnet
 
-SEED = 182
+SEED = 191
 
 
 def test_elnet_lambdas():
@@ -163,12 +163,10 @@ def test_fit_cv():
     y = X.dot(true_betas) + error
     m = Elnet(n_splits=3)
     m.fit(X, y)
-    assert hasattr(m, "lambda_1std_")
-    assert hasattr(m, "lambda_best_")
+    assert hasattr(m, "lambda_1se_")
+    assert hasattr(m, "lambda_max_")
     assert hasattr(m, "cv_mean")
-    assert hasattr(m, "cv_std")
-    assert hasattr(m, "cv_up")
-    assert hasattr(m, "cv_low")
+    assert hasattr(m, "cv_se")
 
 
 def test_predict_default():
@@ -203,3 +201,17 @@ def test_predict_without_cv():
     m.fit(X, y)
     with pytest.raises(Exception):
         m.predict(X)
+
+
+def test_fit_cv_glmnet_comparison():
+    rng = np.random.default_rng(SEED)
+    error = rng.normal(loc=0, scale=1, size=100)
+    X = rng.normal(loc=5, scale=2, size=(100, 4))
+    true_betas = np.array([1, -2, 0.5, 1])
+    y = X.dot(true_betas) + error
+    m = Elnet(n_splits=3, random_state=182, scoring="r2")
+    m.fit(X, y)
+    m2 = ElasticNet(n_splits=3, random_state=182)
+    m2.fit(X, y)
+    np.testing.assert_almost_equal(m.lambda_max_, m2.lambda_max_)
+    np.testing.assert_almost_equal(m.lambda_1se_, m2.lambda_best_[0])